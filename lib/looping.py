7a631cb9110550b8919879f85a02e0c974a6dbcf
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-22 11:14:04 -0500

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 11
+    while num >= 1:
+        num -= 1
+        if num > 1:
+            print(num)
+        elif num == 1:
+            print(num)
+            print("Happy New Year!")
+
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_int_list = list()
+    for i in int_list:
+        new_int_list.append(i ** 2)
+    return new_int_list
+
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    fb_num = 0
+    while fb_num < 100:
+        fb_num += 1
+        if not fb_num % 15:
+            print("FizzBuzz")
+        elif not fb_num % 5:
+            print('Buzz')
+        elif not fb_num % 3:
+            print('Fizz')
+        else :
+            print(fb_num)
